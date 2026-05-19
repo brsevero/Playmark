debc0cb3e48c6fca1865e05c3412459a1fff865e
brsevero <brsevero22@gmail.com>
2026-05-18 23:06:21 -0300

    adicionando main.py + lista de tarefas

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+adult: um booleano que indica se o filme é para adultos
+backdrop_path: o caminho para a imagem de fundo do filme
+genre_ids: um array de inteiros com os ids dos gêneros do filme
+id: o id do filme
+original_language: o idioma original do filme
+original_title: o título original do filme
+overview: a sinopse do filme
+popularity: a popularidade do filme
+poster_path: o caminho para o poster do filme
+release_date: a data de lançamento do filme
+title: o título do filme
+video: um booleano que indica se o filme tem vídeo
+vote_average: a média de votos do filme
+vote_count: o número de votos do filme
+"""
